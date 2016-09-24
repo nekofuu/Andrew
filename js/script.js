94e26386fa7d48edb9d66b3de015897568665ba1
nekofuu <nekofuuhd@gmail.com>
2016-09-24 03:15:52 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var h1 = document.querySelector("h1");
+var btn = document.querySelector("button");
+
+btn.addEventListener("click", function() {
+        h1.textContent = "You're a faggot!";
+        btn.textContent = "TROLOLOL";
+})
